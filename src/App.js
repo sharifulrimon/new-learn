23e83d5338a6e33cc5a7b6693109f1b1cf095720
sharifulrimon <40694130+sharifulrimon@users.noreply.github.com>
2021-07-12 20:23:10 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
+    return (
 
         <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank" >
-        Learn React < /a>  <
-        /header>  <
+        div >
+
+        <
         /div>
-    );
+
+
+    )
+
 }
 
 export default App;
